b278aa7cd6e8acd1cc66b173e6f56ba60703a695
nava <sarvanava@gmail.com>
2023-12-31 10:31:38 -0500

    login page ui changed

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -28,53 +28,62 @@ const Login = () => {
         if (data.status === "success") {
           Cookies.set("user", JSON.stringify(data.user));
 
-
-          if (data.user.user_location ==="Railside"){
-
-              navigate("/commisary");
-
-          }
-          else{
+          if (data.user.user_location === "Railside") {
+            navigate("/commisary");
+          } else {
             navigate("/stores");
           }
         } else {
           setError(data.message);
         }
       } else {
-        setError("An error occured during login.");
+        setError("An error occurred during login.");
       }
     } catch (error) {
-      setError("An error occurred durings login.");
+      setError("An error occurred during login.");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label htmlFor="user_id">User ID:</label>
-          <input
-            type="text"
-            id="user_id"
-            value={user_id}
-            onChange={(e) => setUserid(e.target.value)}
-          />
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="user_id" className="form-label">
+                User ID:
+              </label>
+              <input
+                type="text"
+                id="user_id"
+                className="form-control"
+                value={user_id}
+                onChange={(e) => setUserid(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="user_auth" className="form-label">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="user_auth"
+                className="form-control"
+                value={user_auth}
+                onChange={(e) => setUserauth(e.target.value)}
+              />
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary w-100"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            {error && <p className="mt-3 text-danger text-center">{error}</p>}
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="text"
-            id="user_auth"
-            value={user_auth}
-            onChange={(e) => setUserauth(e.target.value)}
-          />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </form>
+      </div>
     </div>
   );
 };
